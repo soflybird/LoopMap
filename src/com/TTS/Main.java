22a5d13a09d8b99a33d5d1eee15af03a48e23af8
soflybird <74286728+soflybird@users.noreply.github.com>
2021-01-13 08:24:03 -0600

    Update add product

--- src/com/TTS/Main.java
+++ src/com/TTS/Main.java
@@ -2,23 +2,26 @@ package com.TTS;
 import java.util.*;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
 
     private static Scanner input;
+    private static Integer Civic;
 
     public static void main(String[] args) {
 
         Scanner readme = new Scanner(System.in);
         System.out.println("Enter Five Numbers (Press Enter after each):");
 
-        int n1, n2, n3, n4, n5, n6;
+        int n1, n2, n3, n4, n5, n6, n7;
         n1 = readme.nextInt();
         n2 = readme.nextInt();
         n3 = readme.nextInt();
         n4 = readme.nextInt();
         n5 = readme.nextInt();
         n6 = n1 + n2 + n3 + n4 + n5;
+        n7 = n1 * n2 * n3 * n4 * n5;
 
         int[] arr = {n1, n2, n3, n4, n5};
         int smallest = Integer.MAX_VALUE;
@@ -40,9 +43,14 @@ public class Main {
 
             }
             System.out.println("These numbers total: = " + n6);
+
+            System.out.println("The product is: " + n7);
+
             System.out.println("The smallest number is: " + smallest);
+
             System.out.println("The largest number is: " + largest);
+
         }
     }
-}
+    }
 
